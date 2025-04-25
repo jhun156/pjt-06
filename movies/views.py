21d797313a7daf77a834b178e574254869acd50b
박지훈 <qkrwlgns156@naver.com>
2025-04-25 13:56:07 +0900

    @

--- movies/views.py
+++ movies/views.py
@@ -8,37 +8,37 @@ from .serializer import MovieListSerializer, MovieSerializer
 from .serializer import ReviewListSerializer, ReviewSerializer
 
 # Create your views here.
-api_view(['GET'])
+@api_view(['GET'])
 def actor_list(request):
     actors = get_list_or_404(Actor)
     serializer = ActorListSerializer(actors, many=True)
     return Response(serializer.data)
 
-api_view(['GET'])
+@api_view(['GET'])
 def actor_detail(request, actor_pk):
     actor = get_object_or_404(Actor, pk=actor_pk)
     serializer = ActorSerializer(actor)
     return Response(serializer.data)
 
-api_view(['GET'])
+@api_view(['GET'])
 def movie_list(request):
     movies = get_list_or_404(Movie)
     serializer = MovieListSerializer(movies, many=True)
     return Response(serializer.data)
 
-api_view(['GET'])
+@api_view(['GET'])
 def movie_detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = MovieSerializer(movie)
     return Response(serializer.data)
 
-api_view(['GET'])
+@api_view(['GET'])
 def review_list(request):
     reviews = get_list_or_404(Review)
     serializer = ReviewListSerializer(reviews, many=True)
     return Response(serializer.data)
 
-api_view(['GET','PUT','DELETE'])
+@api_view(['GET','PUT','DELETE'])
 def review_detail(request, review_pk):
     review = get_object_or_404(Review, pk=review_pk)
 
@@ -59,7 +59,7 @@ def review_detail(request, review_pk):
         }
         return Response(data, status=status.HTTP_204_NO_CONTENT)
 
-api_view(['POST'])
+@api_view(['POST'])
 def create_review(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
     serializer = ReviewSerializer(data=request.data)
