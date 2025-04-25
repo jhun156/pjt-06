ca5a29bd1e9b44bc1ab93120685a92447d915193
유영훈 <dbdudgns982@gmail.com>
2025-04-25 10:14:29 +0900

    base settings

---
+++ movies/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# GET
+def actor_list(request):
+    pass
+
+# GET
+def actor_detail(request, actor_pk):
+    pass
+
+# GET
+def movie_list(request):
+    pass
+
+# GET
+def movie_detail(request, movie_pk):
+    pass
+
+# GET
+def review_list(request):
+    pass
+
+# GET PUT DELETE
+def review_detail(request, review_pk):
+    pass
+
+# POST
+def create_review(request, movie_pk):
+    pass
